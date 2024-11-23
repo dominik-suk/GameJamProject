1d5450aeb1025d3d8853ec75beaef6b446eea92c
Cedric <cedricgiese@gmail.com>
2024-11-23 13:24:07 +0100

    Added smoother Y Camera movement

--- Assets/Scripts/Platformer Basics/PlatformerCameraTarget.cs
+++ Assets/Scripts/Platformer Basics/PlatformerCameraTarget.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlatformerCameraTarget : MonoBehaviour {
@@ -9,15 +10,20 @@ public class PlatformerCameraTarget : MonoBehaviour {
     }
     private void Update() {
         Vector2 newPosition = parent.position;
-        if(groundedChecker.IsGrounded)
+        float distanceToPlayer = (transform.position - parent.transform.position).magnitude;
+        if (groundedChecker.IsGrounded)
         {
             lastYPos = parent.position.y;
         }
         else
         {
             newPosition.y = lastYPos;
+            if(distanceToPlayer > 2)
+            {
+                lastYPos = Vector2.Lerp(new Vector2(parent.position.x, lastYPos), parent.position, 0.5f).y;
+            }
         }
-        
+
         transform.position = newPosition;
     }
 }

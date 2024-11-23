f738c70759b01eba994a6ba31865375deb28abdf
Fabian <f2abianski@gmail.com>
2024-11-23 21:08:03 +0100

    Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private string mainMenuSceneName;
+    [SerializeField] private GameObject controlsPanel;
+    private void OnEnable() {
+        Time.timeScale = 0;
+    }
+    private void OnDisable() {
+        Time.timeScale = 1;
+    }
+    public void OpenMainMenu(){
+        SceneManager.LoadScene(mainMenuSceneName);
+    }
+
+}

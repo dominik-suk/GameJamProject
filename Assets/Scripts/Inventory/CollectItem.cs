189532e7ac07d6e97538462cd7a3a7feff46b2b7
Fabian <f2abianski@gmail.com>
2024-11-24 16:56:23 +0100

    Level2_V2

--- Assets/Scripts/Inventory/CollectItem.cs
+++ Assets/Scripts/Inventory/CollectItem.cs
@@ -5,17 +5,18 @@ public class CollectItem : MonoBehaviour, IInteractable
 {
     [Header("Settings")]
     [SerializeField] private Item item;
+
     [Header("Events")]
-    private UnityEvent itemCollectedEvent;
-    // [Header("Events")]
-    // [SerializeField] private UnityEvent itemCollectedEvent;
+    [SerializeField] private UnityEvent itemCollectedEvent;
+
     private void Start() {
-        Debug.Log(Inventory.Instance.ContainsItem(item));
-        if(Inventory.Instance.ContainsItem(item)) Destroy(gameObject);
+        // Debug.Log(Inventory.Instance.ContainsItem(item));
+        // if(Inventory.Instance.ContainsItem(item)) Destroy(gameObject);
     }
     public void Interact()
     {
-        Inventory.Instance.CollectItem(item);
+        // Inventory.Instance.CollectItem(item);
+        itemCollectedEvent?.Invoke();
         Destroy(gameObject);
     }
 }
